618b8b16d3f03258845ab26f77c06dd0d8708f5c
SamSJackson <samuel.jackson.scotland2@gmail.com>
2021-12-27 16:11:44 +0000

    round A

--- Kickstart/2020/A.cpp
+++ Kickstart/2020/A.cpp
@@ -1,9 +1,32 @@
 #include <cstring>
 #include <string>
 #include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-	return 0;
+	int testcases, n, budget, maxHouses;
+	int tmp;
+	cin >> testcases;
+	for (int i=0; i < testcases; i++) {
+		cin >> n >> budget;
+		vector<int> houses;
+		for (int j=0; j < n; j++) {
+			cin >> tmp;
+			houses.push_back(tmp);
+		}
+		sort(houses.begin(), houses.end());
+		maxHouses = 0;
+		for (int house : houses) { 
+			if (budget >= house) {
+				maxHouses += 1;
+				budget -= house;
+			} else { 
+				break; 
+			} 
+		}
+		cout << "Case  #" << i+1 << ": " << maxHouses << '\n';
+	}
 }
