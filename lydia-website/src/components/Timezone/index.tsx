3a9716af76adb8a9a2c102a5796fe77c327c5d0c
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-08-13 14:33:17 +0100

    Fixed timezone showing invalid date

--- lydia-website/src/components/Timezone/index.tsx
+++ lydia-website/src/components/Timezone/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-
 import './main.scss'
-
 
 type Props = {
     timeZone: string
@@ -13,30 +11,21 @@ const Timezone : React.FC<Props> = ({
 }) => {
 
     const [time, setTime] = useState<string>(
-        new Date().toLocaleTimeString("en-GB", {timeZone})
+        new Date().toLocaleTimeString('en-GB', {timeZone})
     );
 
     const refreshClock = () => {
-        handleTimeZone(Date(), timeZone);
+        const newTime = new Date();
+
+        setTime(newTime.toLocaleTimeString('en-GB', {timeZone}));
     };
-
-    const handleTimeZone = (date : string, timeZone : string) => {
-        const newTime = new Date(
-            new Date(date).toLocaleString('en-GB', {
-                timeZone,
-            }),
-        );
-
-        setTime(newTime.toLocaleTimeString());
-    }
 
     useEffect(() => {
         const timerId = setInterval(refreshClock, 1000);
         return function cleanup() {
             clearInterval(timerId);
         };
-    }, []);
-
+    });
 
     return (
         <div className="timezone-container">
